ded941c508cf5e4b82e66d38d078e1818d76407e
Dennis Klein <d.klein@gsi.de>
2018-03-19 15:55:54 +0100

    Initial commit

---
+++ cmake/AsiofiInit.cmake
@@ -0,0 +1,100 @@
+################################################################################
+#    Copyright (C) 2018 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    #
+#                                                                              #
+#              This software is distributed under the terms of the             #
+#              GNU Lesser General Public Licence (LGPL) version 3,             #
+#                  copied verbatim in the file "LICENSE"                       #
+################################################################################
+
+### PUBLIC
+
+# Defines some variables with console color escape sequences
+if(NOT WIN32 AND NOT DISABLE_COLOR)
+  string(ASCII 27 Esc)
+  set(CR       "${Esc}[m")
+  set(CB       "${Esc}[1m")
+  set(Red      "${Esc}[31m")
+  set(Green    "${Esc}[32m")
+  set(Yellow   "${Esc}[33m")
+  set(Blue     "${Esc}[34m")
+  set(Magenta  "${Esc}[35m")
+  set(Cyan     "${Esc}[36m")
+  set(White    "${Esc}[37m")
+  set(BRed     "${Esc}[1;31m")
+  set(BGreen   "${Esc}[1;32m")
+  set(BYellow  "${Esc}[1;33m")
+  set(BBlue    "${Esc}[1;34m")
+  set(BMagenta "${Esc}[1;35m")
+  set(BCyan    "${Esc}[1;36m")
+  set(BWhite   "${Esc}[1;37m")
+endif()
+
+# set_asiofi_cmake_policies()
+#
+# Sets CMake policies.
+macro(set_fairroot_cmake_policies)
+  # Find more details to each policy with cmake --help-policy CMPXXXX
+  foreach(policy
+    CMP0025 # Compiler id for Apple Clang is now ``AppleClang``.
+    CMP0028 # Double colon in target name means ALIAS or IMPORTED target.
+    CMP0042 # MACOSX_RPATH is enabled by default.
+    CMP0048 # The ``project()`` command manages VERSION variables.
+    CMP0054 # Only interpret ``if()`` arguments as variables or keywords when unquoted.
+    CMP0068 # ``RPATH`` settings on macOS do not affect ``install_name``.
+  )
+    if(POLICY ${policy})
+      cmake_policy(SET ${policy} NEW)
+    endif()
+  endforeach()
+endmacro()
+
+find_package(Git)
+# get_git_version([DEFAULT_VERSION version] [DEFAULT_DATE date] [OUTVAR_PREFIX prefix])
+#
+# Sets variables #prefix#_VERSION, #prefix#_GIT_VERSION, #prefix#_DATE, #prefix#_GIT_DATE
+function(get_git_version)
+  cmake_parse_arguments(ARGS "" "DEFAULT_VERSION;DEFAULT_DATE;OUTVAR_PREFIX" "" ${ARGN})
+
+  if(NOT ARGS_OUTVAR_PREFIX)
+    set(ARGS_OUTVAR_PREFIX ASIOFI)
+  endif()
+
+  if(GIT_FOUND AND EXISTS ${CMAKE_SOURCE_DIR}/.git)
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v-*"
+      OUTPUT_VARIABLE ${ARGS_OUTVAR_PREFIX}_GIT_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    )
+    if(${ARGS_OUTVAR_PREFIX}_GIT_VERSION)
+      # cut first two characters "v-"
+      string(SUBSTRING ${${ARGS_OUTVAR_PREFIX}_GIT_VERSION} 2 -1 ${ARGS_OUTVAR_PREFIX}_GIT_VERSION)
+    endif()
+    execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --format=%cd
+      OUTPUT_VARIABLE ${ARGS_OUTVAR_PREFIX}_GIT_DATE
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    )
+  endif()
+
+  if(NOT ${ARGS_OUTVAR_PREFIX}_GIT_VERSION)
+    if(ARGS_DEFAULT_VERSION)
+      set(${ARGS_OUTVAR_PREFIX}_GIT_VERSION ${ARGS_DEFAULT_VERSION})
+    else()
+      set(${ARGS_OUTVAR_PREFIX}_GIT_VERSION 0.0.0.0)
+    endif()
+  endif()
+  
+  if(NOT ${ARGS_OUTVAR_PREFIX}_GIT_DATE)
+    if(ARGS_DEFAULT_DATE)
+      set(${ARGS_OUTVAR_PREFIX}_GIT_DATE ${ARGS_DEFAULT_DATE})
+    else()
+      set(${ARGS_OUTVAR_PREFIX}_GIT_DATE "Thu Jan 1 00:00:00 1970 +0000")
+    endif()
+  endif()
+
+  # return values
+  set(${ARGS_OUTVAR_PREFIX}_VERSION ${CMAKE_MATCH_0} PARENT_SCOPE)
+  set(${ARGS_OUTVAR_PREFIX}_DATE ${${ARGS_OUTVAR_PREFIX}_GIT_DATE} PARENT_SCOPE)
+  set(${ARGS_OUTVAR_PREFIX}_GIT_VERSION ${${ARGS_OUTVAR_PREFIX}_GIT_VERSION} PARENT_SCOPE)
+  set(${ARGS_OUTVAR_PREFIX}_GIT_DATE ${${ARGS_OUTVAR_PREFIX}_GIT_DATE} PARENT_SCOPE)
+endfunction()
