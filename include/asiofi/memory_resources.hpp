7c5a6f7b6a172f582b3e962e665dbc96c05cbb92
Dennis Klein <d.klein@gsi.de>
2018-11-20 23:54:27 +0100

    Make allocator thread safe

--- include/asiofi/memory_resources.hpp
+++ include/asiofi/memory_resources.hpp
@@ -9,7 +9,8 @@
 #ifndef ASIOFI_MEMORY_RESOURCES_HPP
 #define ASIOFI_MEMORY_RESOURCES_HPP
 
-#include <boost/container/pmr/unsynchronized_pool_resource.hpp>
+#include <atomic>
+#include <boost/container/pmr/synchronized_pool_resource.hpp>
 #include <cstring>
 #include <iomanip>
 #include <iostream>
@@ -22,21 +23,23 @@ namespace asiofi
 
 /**
  * @struct allocated_pool_resource allocators.hpp <asiofi/memory_resources.hpp>
- * @brief Works just like boost::container::pmr::unsynchronized_pool_resource, but physically
+ * @brief Works just like boost::container::pmr::synchronized_pool_resource, but physically
  *        allocates new buffers.
  */
-struct allocated_pool_resource : boost::container::pmr::unsynchronized_pool_resource
+struct allocated_pool_resource : boost::container::pmr::synchronized_pool_resource
 {
   allocated_pool_resource()
-  : unsynchronized_pool_resource()
+  : synchronized_pool_resource()
+  , m_hit(0)
+  , m_total(0)
   {
   }
 
-  // ~allocated_pool_resource() override
-  // {
-    // double fast_alloc_rate = (m_hit * 100.) / (m_total * 1.);
-    // std::cout << std::fixed << std::setprecision(2) << fast_alloc_rate << "% reused allocations of " << m_total << " total allocations" << std::endl;
-  // }
+  ~allocated_pool_resource() override
+  {
+    double fast_alloc_rate = (m_hit * 100.) / (m_total * 1.);
+    std::cout << std::fixed << std::setprecision(2) << fast_alloc_rate << "% reused allocations of " << m_total << " total allocations" << std::endl;
+  }
 
   protected:
   auto do_allocate(std::size_t bytes, std::size_t alignment) -> void* override
@@ -44,21 +47,23 @@ struct allocated_pool_resource : boost::container::pmr::unsynchronized_pool_reso
     auto ptr = boost::container::pmr::unsynchronized_pool_resource::do_allocate(bytes, alignment);
 
     if (m_allocated.insert(ptr).second) {
-      std::memset(ptr, 0, bytes);
+      std::memset(ptr, 0, bytes); // TODO see if we need stronger page pinning here
       // std::cout << "allocated: ptr=" << ptr << ", size=" << bytes << std::endl;
     } else {
       // std::cout << "allocated (fast): ptr=" << ptr << ", size=" << bytes << std::endl;
+      ++m_hit;
     }
+    ++m_total;
 
     return ptr;
   }
 
-  // auto do_deallocate(void* p, std::size_t bytes, std::size_t alignment) -> void override
-  // {
-    // boost::container::pmr::unsynchronized_pool_resource::do_deallocate(p, bytes, alignment);
-//
+  auto do_deallocate(void* p, std::size_t bytes, std::size_t alignment) -> void override
+  {
+    boost::container::pmr::unsynchronized_pool_resource::do_deallocate(p, bytes, alignment);
+
     // std::cout << "deallocated: ptr=" << p << ", size=" << bytes << std::endl;
-  // }
+  }
 
   // auto do_is_equal(const memory_resource& other) const -> bool noexcept
   // {
@@ -67,6 +72,7 @@ struct allocated_pool_resource : boost::container::pmr::unsynchronized_pool_reso
 
   private:
   std::unordered_set<void*> m_allocated;
+  std::atomic<size_t> m_hit, m_total;
 }; /* struct allocated_pool_memory_resource */
 
 } /* namespace asiofi */
