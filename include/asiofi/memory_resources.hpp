2d84c84ab34b63752ba4d796fbcaa6b1461f1662
Dennis Klein <d.klein@gsi.de>
2019-03-26 16:19:36 +0100

    Implement asiofi::registered_memory_resource

--- include/asiofi/memory_resources.hpp
+++ include/asiofi/memory_resources.hpp
@@ -9,14 +9,19 @@
 #ifndef ASIOFI_MEMORY_RESOURCES_HPP
 #define ASIOFI_MEMORY_RESOURCES_HPP
 
+#include <asiofi/memory_region.hpp>
 #include <atomic>
+#include <boost/container/pmr/global_resource.hpp>
+#include <boost/container/pmr/memory_resource.hpp>
+#include <boost/container/pmr/monotonic_buffer_resource.hpp>
 #include <boost/container/pmr/synchronized_pool_resource.hpp>
 #include <cstring>
 #include <iomanip>
 #include <iostream>
-#include <vector>
+#include <sys/mman.h>
 #include <unordered_set>
 #include <utility>
+#include <vector>
 
 namespace asiofi
 {
@@ -74,6 +79,65 @@ struct allocated_pool_resource : boost::container::pmr::synchronized_pool_resour
   std::unordered_set<void*> m_allocated;
   std::atomic<size_t> m_hit, m_total;
 }; /* struct allocated_pool_memory_resource */
+
+/**
+ * @struct registered_memory_resource memory_resources.hpp <asiofi/memory_resources.hpp>
+ * @brief Works just like monotonic memory resource, but adds mlock and OFI
+ *        memory region registration to the first upstream allocation.
+ */
+struct registered_memory_resource : boost::container::pmr::memory_resource
+{
+  registered_memory_resource(const asiofi::domain& domain,
+      std::size_t size,
+      boost::container::pmr::memory_resource* upstream = boost::container::pmr::get_default_resource())
+  : m_ptr(upstream->allocate(size))
+  , m_size(size)
+  , m_region(create_region(domain))
+  , m_upstream(m_ptr, m_size, upstream)
+  {
+    //std::cout << "m_ptr=" << m_ptr << "m_size=" << m_size << std::endl;
+  }
+
+  ~registered_memory_resource()
+  {
+    munlock(m_ptr, m_size);
+  }
+
+  auto get_region() -> asiofi::memory_region&
+  {
+    return m_region;
+  }
+
+  protected:
+  auto do_allocate(std::size_t bytes, std::size_t alignment) -> void* override
+  {
+    return m_upstream.allocate(bytes, alignment);
+  }
+
+  auto do_deallocate(void* p, std::size_t bytes, std::size_t alignment) -> void override
+  {
+    return m_upstream.deallocate(p, bytes, alignment);
+  }
+
+  auto do_is_equal(const memory_resource& other) const noexcept -> bool
+  {
+    return m_upstream.is_equal(other);
+  }
+
+  private:
+  void* m_ptr;
+  std::size_t m_size;
+  asiofi::memory_region m_region;
+  boost::container::pmr::monotonic_buffer_resource m_upstream;
+
+  auto create_region(const asiofi::domain& domain) -> asiofi::memory_region
+  {
+    mlock(m_ptr, m_size);
+    return asiofi::memory_region(domain,
+            boost::asio::mutable_buffer(m_ptr, m_size),
+            asiofi::mr::access::recv | asiofi::mr::access::send);
+  }
+}; /* struct registered_memory_resource */
 
 } /* namespace asiofi */
 
