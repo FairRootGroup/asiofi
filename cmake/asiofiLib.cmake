1f24ae5d9c47754d61c6a70f17ba8e1d25c5aba5
Dennis Klein <d.klein@gsi.de>
2019-05-14 18:38:26 +0200

    Colorize ninja output

--- cmake/asiofiLib.cmake
+++ cmake/asiofiLib.cmake
@@ -150,6 +150,14 @@ macro(set_asiofi_defaults)
       set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${PROJECT_INSTALL_LIBDIR}")
     endif()
   endif()
+
+  if(CMAKE_GENERATOR STREQUAL "Ninja" AND
+     ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9) OR
+      (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5)))
+    # Force colored warnings in Ninja's output, if the compiler has -fdiagnostics-color support.
+    # Rationale in https://github.com/ninja-build/ninja/issues/814
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
+  endif()
 endmacro()
 
 function(join VALUES GLUE OUTPUT)
